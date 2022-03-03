ce239eddc67016fe84d214d18472e06900904236
dolks <dylanoelkers165@gmail.com>
2022-03-02 20:27:07 -0500

    Added base of node class to be used in pathfinding

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node
+{
+    public Vector2Int coordinates;
+    public bool isSearchable;
+    public bool isExplored;
+    public bool isPath;
+    public Node parent;
+
+    public Node(Vector2Int coordinates, bool isSearchable)
+    {
+        this.coordinates = coordinates;
+        this.isSearchable = isSearchable;
+    }
+}
