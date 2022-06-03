585250d0fcf38c516928d626c0f28f56984845e6
dolks <dylanoelkers165@gmail.com>
2022-06-02 23:42:45 -0400

    added build delay for towers, base of post processing

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 50;
+    [SerializeField] int buildDelay = 1;
     Bank bank;
     // Start is called before the first frame update
     void Start()
     {
         bank = FindObjectOfType<Bank>();
+        StartCoroutine(Build());
     }
 
     public bool Place(Tower tower, Vector3 position)
     {
         bank = FindObjectOfType<Bank>();
-        if (bank == null) {return false; }
+        if (bank == null) { return false; }
         if (bank.CurrentAmount >= cost)
         {
             Instantiate(tower.gameObject, position, Quaternion.identity);
@@ -24,5 +26,27 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
     }
 }
